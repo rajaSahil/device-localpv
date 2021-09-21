4b1517da2b38d7d6eadd74556694d0fb4d25a27a
Sahil Raja <sahilraja242@gmail.com>
2021-09-21 08:25:41 +0530

    Updated storage volume crd spec
    Signed-off-by: Sahil Raja <sahilraja242@gmail.com>

--- pkg/apis/openebs.io/scp/v1alpha1/storage-volume.go
+++ pkg/apis/openebs.io/scp/v1alpha1/storage-volume.go
@@ -49,147 +49,25 @@ type StorageVolumeList struct {
 // +k8s:openapi-gen=true
 type StorageVolumeSpec struct {
 	// Capacity of the volume
-	// +kubebuilder:validation:Required
-	// +kubebuilder:validation:MinLength=1
 	Capacity resource.Quantity `json:"capacity"`
 
-	// ProtocolConfiguration represents protocol specific configurations for NVME
-	// +nullable
-	ProtocolConfiguration ProtocolConfiguration `json:"ProtocolConfiguration,omitempty"`
+	// Capabilities stores the capabilities of a volume that is passed through storage class
+	Capabilities *Capabilities `json:"capabilities"`
 
-	// VolumeType stores configuration that is passed through storage class
-	VolumeType VolumeType `json:"volumeType"`
+	// Parameters holds the configuration that is required at the time of volume creation
+	// Can be nvme or iscsi specific configurations
+	// +optional
+	Parameters interface{} `json:"parameters,omitempty"`
 
 	// Affinity is a group of affinity scheduling rules
-	// +nullable
+	// +optional
 	Affinity Affinity `json:"affinity,omitempty"`
 
-	// StorageCohortID represents the cohort in which volume has to be provisioned
-	StorageCohortName string `json:"storageCohortName"`
+	// StorageCohortReference points to the pre-existing StorageCohort resource
+	StorageCohortReference v1.ObjectReference `json:"storageCohortReference"`
 
-	// StoragePoolID represents the pool in which volume has to be created
-	StoragePoolName string `json:"StoragePoolName"`
-}
-
-// ProtocolConfiguration represents protocol specific configurations for NVME
-type ProtocolConfiguration struct {
-	// AllowedHosts represents the allowed hosts
-	AllowedHosts []string `json:"allowedHosts,omitempty"`
-}
-
-// VolumeType stores configuration of volumes that is passed through storage class
-type VolumeType struct {
-	// Different Capabilities for a volume
-	Capabilities *Capabilities `json:"capabilities"`
-}
-
-// Capabilities of the volume
-type Capabilities struct {
-	// DataStorage capabilities
-	DataStorageCapabilities *DataStorageCapabilities `json:"dataStorageCapabilities"`
-
-	// Data security capabilities
-	DataSecurityCapabilities *DataSecurityCapabilities `json:"dataSecurityCapabilities,omitempty"`
-
-	// IO connectivity capabilities for the volume
-	IOConnectivityCapabilities *IOConnectivityCapabilities `json:"IOConnectivityCapabilities,omitempty"`
-
-	// IO performance capabilities
-	IOPerformanceCapabilities *IOPerformanceCapabilities `json:"IOPerformanceCapabilities,omitempty"`
-
-	MaxIOOperationsPerSecondPerTerabyte string `json:"MaxIOOperationsPerSecondPerTerabyte,omitempty"`
-
-	// storage tier to be used for volume creations. eg(Platinum, Gold, Silver)
-	StorageTier StorageTier `json:"storageTier"`
-
-	DataProtectionCapabilities *DataProtectionCapabilities `json:"DataProtectionCapabilities,omitempty"`
-}
-
-type StorageTier string
-
-const (
-	// StorageTierPlatinum indicates that the tier to be used for volume is platinum
-	StorageTierPlatinum StorageTier = "P1"
-
-	// StorageTierGold indicates that the tier to be used for volume is gold
-	StorageTierGold StorageTier = "P2"
-
-	// StorageTierSilver indicates that the tier to be used for volume is silver
-	StorageTierSilver StorageTier = "P3"
-)
-
-// DataStorageCapabilities defines data storage capabilities for a volume
-type DataStorageCapabilities struct {
-	//AccessModes contains the actual access modes for the volume
-	AccessModes StorageVolumeAccessMode `json:"accessModes"`
-
-	// ProvisioningPolicy defines provisioning policy type
-	ProvisioningPolicy ProvisioningPolicy `json:"provisioningPolicy"`
-
-	// RecoveryTimeObjectives to be supported or not
-	RecoveryTimeObjectives RecoveryTimeObjectives `json:"RecoveryTimeObjectives"`
-
-	// Compression to be supported or not, if yes the algorithm
-	//	+nullable
-	Compression *CompressionAlgorithm `json:"compression,omitempty"`
-
-	// Deduplication to be supported or not
-	// +nullable
-	Deduplication *bool `json:"deduplication,omitempty"`
-}
-
-type StorageVolumeAccessMode string
-
-const (
-	// can be mounted in read/write mode to exactly 1 host
-	ReadWriteOnce StorageVolumeAccessMode = "ReadWriteOnce"
-	// can be mounted in read-only mode to many hosts
-	ReadOnlyMany StorageVolumeAccessMode = "ReadOnlyMany"
-	// can be mounted in read/write mode to many hosts
-	ReadWriteMany StorageVolumeAccessMode = "ReadWriteMany"
-)
-
-
-type RecoveryTimeObjectives string
-
-const (
-	// OnlineActive indicates that the recovery time objective for a volume is online active
-	OnlineActive RecoveryTimeObjectives = "OnlineActive"
-
-	// OnlinePassive indicates that the recovery time objective for a volume is online active
-	OnlinePassive RecoveryTimeObjectives = "OnlinePassive"
-)
-
-type ProvisioningPolicy string
-
-const (
-	// ProvisioningPolicyThick indicates that the provisioning policy is thick
-	ProvisioningPolicyThick ProvisioningPolicy = "Thick"
-
-	// ProvisioningPolicyThin indicates that the provisioning policy is thin
-	ProvisioningPolicyThin ProvisioningPolicy = "Thin"
-)
-
-type CompressionAlgorithm string
-
-const(
-	HuffmanCompression CompressionAlgorithm = "huffman"
-)
-
-type DataSecurityCapabilities struct {
-	MediaEncryption *bool `json:"MediaEncryption,omitempty"`
-}
-
-type IOConnectivityCapabilities struct {
-	AccessProtocols string `json:"accessProtocols,omitempty"`
-}
-
-type IOPerformanceCapabilities struct {
-	AverageIOOperationLatencyMicroseconds string `json:"averageIOOperationLatencyMicroseconds,omitempty"`
-}
-
-type DataProtectionCapabilities struct {
-	// TODO: Decide the Fields
+	// StoragePoolReference points to the pre-existing StoragePool resource
+	StoragePoolReference v1.ObjectReference `json:"storagePoolReference"`
 }
 
 // Affinity is a group of affinity scheduling rules
@@ -216,100 +94,100 @@ type Affinity struct {
 	TopologySpreadConstraint []v1.TopologySpreadConstraint `json:"topologySpreadConstraint,omitempty"`
 }
 
+// VolumeAffinityTerm specifies affinity requirements for a StorageVolume
 type VolumeAffinityTerm struct {
+	// TopologyKey is the key of cohort labels. StorageCohort that have a label with this key
+	// and identical values are considered to be in the same topology.
+	// +optional
 	TopologyKey string `json:"topologyKey,omitempty"`
 
+	// A label query over a set of resources, in this case storage volume.
+	// +optional
 	LabelSelector *metav1.LabelSelector `json:"labelSelector,omitempty"`
 }
 
 // StorageVolumeStatus defines the observed state of StorageVolume
-// +k8s:openapi-gen=true
 type StorageVolumeStatus struct {
 	// Phase defines the state of the volume
-	Phase StorageVolumePhase `json:"phase"`
+	Condition []StorageVolumeCondition `json:"condition,omitempty"`
 
 	// Capacity stores total, used and available size
-	// +nullable
+	// +optional
 	Capacity Capacity `json:"capacity,omitempty"`
 
-	// NVMESpec defines the nvme spec i.e nql, targetIP and status
-	// +nullable
-	NVMESpec []NVMESpec `json:"NVMESpec,omitempty"`
+	// TargetInfo defines the nvme spec i.e nql, targetIP and status
+	// +optional
+	TargetInfo []TargetInfo `json:"targetInfo,omitempty"`
 }
+
+// StorageVolumeCondition contains condition information for a StorageVolume.
+type StorageVolumeCondition struct {
+	// Type is the type of the condition.
+	Type StorageVolumeConditionType `json:"type"`
+
+	// Status is the status of the condition.
+	// Can be True, False, Unknown.
+	Status v1.ConditionStatus `json:"status"`
+
+	// Last time the condition transitioned from one status to another.
+	// +optional
+	LastTransitionTime metav1.Time `json:"lastTransitionTime,omitempty"`
+
+	// Last time we got an update on a given condition.
+	// +optional
+	LastHeartbeatTime metav1.Time `json:"lastHeartbeatTime,omitempty"`
+
+	// Unique, one-word, CamelCase reason for the condition's last transition.
+	// +optional
+	Reason string `json:"reason,omitempty"`
+
+	// Human-readable message indicating details about last transition.
+	// +optional
+	Message string `json:"message,omitempty"`
+}
+
+// StorageVolumeConditionType is a valid value for StorageVolumeCondition.Type
+type StorageVolumeConditionType string
+
+const (
+	// StorageVolumeConditionTypePending indicates pending status of the StorageVolume object
+	StorageVolumeConditionTypePending StorageVolumeConditionType = "Pending"
+
+	// StorageVolumeConditionTypeScheduled represents scheduled StorageVolume object
+	StorageVolumeConditionTypeScheduled StorageVolumeConditionType = "Scheduled"
+
+	// StorageVolumeConditionTypeReady represents StorageVolume object is in ready state
+	StorageVolumeConditionTypeReady StorageVolumeConditionType = "Ready"
+)
 
 // Capacity defines total, used and available size
 type Capacity struct {
 	// Total represents total size of volume
+	// +optional
 	Total resource.Quantity `json:"total,omitempty"`
 
 	// Used represents used size of the volume
+	// +optional
 	Used resource.Quantity `json:"used,omitempty"`
 
 	// Available represents available size of the volume
+	// +optional
 	Available resource.Quantity `json:"available,omitempty"`
 
-	// Error denotes the error occurred during provisioning a volume.
-	// Error field should only be set when State becomes Failed.
-	Error *StorageVolumeError `json:"error,omitempty"`
 }
 
-// NVMESpec represents nvme spec for the volume
-type NVMESpec struct {
+// TargetInfo represents nvme spec for the volume
+type TargetInfo struct {
 
 	// Nqn represents target nvme Qualified Name.combination of nodeBase
+	// +optional
 	Nqn string `json:"nqn,omitempty"`
 
 	// TargetIP IP of the NVME target
+	// +optional
 	TargetIP string `json:"targetIP,omitempty"`
 
 	// Status of the nvme target
+	// +optional
 	Status string `json:"status,omitempty"`
 }
-
-// StorageVolumePhase represents the current phase of StorageVolume.
-type StorageVolumePhase string
-
-const (
-	// StorageVolumePhasePending indicates that the StorageVolume is still waiting for
-	// the StorageVolume to be created and bound
-	StorageVolumePhasePending StorageVolumePhase = "Pending"
-
-	// StorageVolumePhaseFailed indicates that the StorageVolume provisioning
-	// has failed
-	StorageVolumePhaseFailed StorageVolumePhase = "Failed"
-
-	// StorageVolumePhaseReady indicates that the StorageVolume provisioning
-	// has Created
-	StorageVolumePhaseReady StorageVolumePhase = "Ready"
-
-	// StorageVolumePhaseDeleting indicates the the StorageVolume is de-provisioned
-	StorageVolumePhaseDeleting StorageVolumePhase = "Deleting"
-
-	// StorageVolumePhaseScheduled indicates the the StorageVolume is scheduled
-	StorageVolumePhaseScheduled StorageVolumePhase = "Scheduled"
-
-	// StorageVolumePhaseHealthy indicates the the StorageVolume is healthy
-	StorageVolumePhaseHealthy StorageVolumePhase = "Healthy"
-
-	// StorageVolumePhaseUnhealthy indicates the the StorageVolume is unhealthy
-	StorageVolumePhaseUnhealthy StorageVolumePhase = "Unhealthy"
-)
-
-// StorageVolumeError specifies the error occurred during volume provisioning.
-type StorageVolumeError struct {
-	Code    StorageVolumeErrorCode `json:"code,omitempty"`
-	Message string          `json:"message,omitempty"`
-}
-
-// StorageVolumeErrorCode represents the error code to represent
-// specific class of errors.
-type StorageVolumeErrorCode string
-
-const (
-	// Internal represents system internal error.
-	Internal StorageVolumeErrorCode = "Internal"
-	// InsufficientCapacity represent device doesn't
-	// have enough capacity to fit the volume request.
-	InsufficientCapacity StorageVolumeErrorCode = "InsufficientCapacity"
-)
-
